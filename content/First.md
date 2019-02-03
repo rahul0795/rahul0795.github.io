41f8757ec4e5fd9a546aee15b397f6e5e4c3adc3
rahul <rahul.jha0795@gmail.com>
2019-02-03 19:15:07 +0530

    f

---
+++ content/First.md
@@ -0,0 +1,23 @@
+Title: Notebook to MD
+Date: 2019-01-01 10:20
+Category: Education
+
+
+
+
+
+```python
+%matplotlib inline
+import matplotlib.pyplot as plt
+plt.plot([1,2,3],[1,2,3],color = "b", marker = "o")
+plt.show()
+```
+
+
+![png](output_0_0.png)
+
+
+
+```python
+
+```
---
+++ pelicanconf.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Rahul'
+SITENAME = 'My Blog'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Asia/Kolkata'
+
+DEFAULT_LANG = 'English'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+"""LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         )
+"""
+# Social widget
+SOCIAL = (('LinkedIn', 'https://www.linkedin.com/in/rahul-jha-bbab34135/'),
+          ('GitHub', 'https://github.com/rahul0795'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
